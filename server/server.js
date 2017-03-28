aba5478841ae87ad0b44667bad2256805835c413
Harmony D <antancia@gmail.com>
2017-03-28 19:36:20 -0400

    additional configuration changes with express

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, '../dist')))
+app.use('/dist', express.static(path.join(__dirname, '../dist')))
 
 app.use('/assets', express.static(path.join(__dirname, '../assets')))
 
